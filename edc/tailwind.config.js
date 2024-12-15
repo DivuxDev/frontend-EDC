7d1b78e7a4d5a8b3becd23ae853ae31ec544ca18
cesartg7 <cesartorregonzalez@gmail.com>
2024-12-15 18:41:05 +0100

    Instalación de tailwind

---
+++ edc/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Escanea todos los archivos Vue y JS/TS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
