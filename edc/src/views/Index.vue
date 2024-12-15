9dfec7d047865b076c7789be29ff0cbe8cdae9c3
cesartg7 <cesartorregonzalez@gmail.com>
2024-12-15 20:18:37 +0100

    Creación de la vista de index

---
+++ edc/src/views/Index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-slate-50">
+    <h1>Bienvenido a mi sitio web</h1>
+    <p>Este es el contenido principal de la página de inicio.</p>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script setup>
+
+</script>
